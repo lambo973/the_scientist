4b30fecec6c61ce4e0fe654ed914a9842b79941d
WEBSOLUTE\a.lambertini <alambertini@websolute.it>
2018-11-29 16:05:01 +0100

    commit

--- app/Http/Controllers/ListArticlesController.php
+++ app/Http/Controllers/ListArticlesController.php
@@ -1,13 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use LaravelDay\Article\UseCase\ListArticles\ListArticles;
 
 class ListArticlesController extends Controller
 {
     public function __invoke()
     {
-        return response()->json([]);
+        $listArticles = new ListArticles();
+        $articles = $listArticles();
+
+        return response()->json($articles);
     }
 }
